ef32654f8e7cf66f9bf77c4f357d5b8fb0f7350e
Alec M <alecmattu1234@gmail.com>
2020-12-14 13:31:35 -0500

    Fix nonexisting replaceAll method

--- assets/templater.js
+++ assets/templater.js
@@ -3,7 +3,7 @@ function replace(str, request) {
   var name = (request.name && typeof(request.name) === "string" ? request.name.substr(0, 20) : "");
 
   // Replace Name
-  str = str.replaceAll("{{name}}", name);
+  str = str.replace("{{name}}", name);
 
   // Return
   return str;
