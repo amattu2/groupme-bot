666607d3501bf8fe106e2606c13ad9821ee66eca
Alec M <alecmattu1234@gmail.com>
2020-12-17 19:07:44 -0500

    Trim whitespace in name template

--- assets/templater.js
+++ assets/templater.js
@@ -3,7 +3,7 @@ function replace(str, request) {
   var name = (request.name && typeof(request.name) === "string" ? request.name.substr(0, 20) : "");
 
   // Replace Name
-  str = str.replace("{{name}}", name);
+  str = str.replace("{{name}}", name.trim());
 
   // Return
   return str;
