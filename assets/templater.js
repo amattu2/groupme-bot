41b1af6233155199f29c5aa71fd5268560d5b74d
Alec M <alecmattu1234@gmail.com>
2020-12-17 19:08:29 -0500

    Add fake GroupMe tag to name template

--- assets/templater.js
+++ assets/templater.js
@@ -1,6 +1,6 @@
 function replace(str, request) {
   // Placeholder values
-  var name = (request.name && typeof(request.name) === "string" ? request.name.substr(0, 20) : "");
+  var name = (request.name && typeof(request.name) === "string" ? "@" + request.name.substr(0, 20) : "");
 
   // Replace Name
   str = str.replace("{{name}}", name.trim());
